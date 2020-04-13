f2359ff990964d49a27d1875701eaacf249e9780
soichikamiya <kamiyasoichi@hotmail.co.jp>
2020-04-14 02:03:33 +0900

    sample_arrayファイル追加＆内容の修正

---
+++ sample_array.rb
@@ -0,0 +1,5 @@
+array = %w(1 2 3 4 5 6 7 8 9 10)
+oddNum = array.each_slice(2).map(&:first)
+evenNum = array.each_slice(2).map(&:last)
+oddNum.each { |n| puts "#{n.to_s} は奇数です" }
+evenNum.each { |n| puts "#{n.to_s} は偶数です" }
